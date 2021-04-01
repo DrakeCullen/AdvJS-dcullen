ad3703774ecb360b35cb15a92d5bcced76ddda3c
Drake C <dpcullen@mavs.coloradomesa.edu>
2021-04-01 11:43:37 -0600

    Update home screen

--- cloud-jumper/src/Components/HomeScreen.js
+++ cloud-jumper/src/Components/HomeScreen.js
@@ -8,12 +8,19 @@ class HomeScreen extends React.Component {
 
     render() {
         return (
-            <div className="game-area" style={{ width: Constants.WIDTH, height: Constants.HEIGHT }}>
-                <div className="text-center">
-                    <button type="button" className="btn btn-success btn-lg" onClick={this.props.startGame}>Play</button>
+            <div className="game-area text-center" style={{ width: Constants.WIDTH, height: Constants.HEIGHT }}>
+                <h1 className="title mt-3">Cloud Jumper</h1>
+                <h2 className="leader-small mt-3">Created By:</h2>
+                <h2 className="leader-small mt-2">Drake Cullen</h2>
+                <img className="bunny-logo mt-4" src={window.location.origin + '/bunny-right.png'}></img>
+                <div>
+                    <button type="button" className="btn btn-success btn-lg mt-4" onClick={this.props.startGame}>Play</button>
                 </div>
-                <div className="text-center">
-                    <button type="button" className="btn btn-info bt-md" onClick={this.props.viewLeaderboard}>View Leaderboard</button>
+                <div>
+                    <button type="button" className="btn btn-primary bt-md mt-3" onClick={this.props.viewLeaderboard}>View Leaderboard</button>
+                </div>
+                <div>
+                    <button type="button" className="btn btn-info bt-md mt-3" onClick={this.props.viewLeaderboard}>Instructions</button>
                 </div>
             </div>
         );
